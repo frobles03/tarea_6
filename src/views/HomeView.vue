f5a18253f69dbc34c4a157fb27d4fe394bac6105
f.robles03 <f.robles03@ufromail.cl>
2022-11-10 23:46:17 -0300

    Se agrego el avance el proyecto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>bienvenido a la tarea 6</h1>
+    <div class="post" v-for="job in jobs" v-bind:key="job._id">
+      <h3> {{ job._id }}</h3>
+      <h3> {{ job.title }}</h3>
+    </div>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      jobs: []
+    }
+  },
+
+  mounted() {
+    let vue = this;
+    axios.get('https://jobs-demo-api-js.herokuapp.com/jobs')
+      .then(function (response) {
+        vue.jobs = response.data;
+        console.log(vue.jobs)
+      })
   }
 }
 </script>
